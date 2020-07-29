97c5d5ccbdb7542912c4d5edec389182a6946685
Jeff Huang <jeffshuang2@gmail.com>
2020-07-29 13:57:02 -0700

    Connected to MongoDB and installed two more dependencies.

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb+srv://jhuang2:kpylVvc8X0eV3eP7@react-graphql-auth.ri9nr.mongodb.net/react-graphql-auth?retryWrites=true&w=majority';
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
