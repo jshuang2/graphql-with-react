96c6693870aff5316fea8662d34fd25c049bc0d8
Jeff Huang <jeffshuang2@gmail.com>
2020-07-31 14:00:30 -0700

    Hooked up an initial React client UI. Created first few components and imported Apollo.

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -35,7 +35,7 @@ const mutation = new GraphQLObjectType({
                 password: { type: GraphQLString }
             },
             resolve(parentValue, { email, password }, req) {
-                return AuthService.login({ email, password, req });
+                return AuthService.login({ email: email, password: password, req: req });
             }
         }
     }
