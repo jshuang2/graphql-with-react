efb66608d623e34f3d351b1304794a43bc7488aa
Jeff Huang <jeffshuang2@gmail.com>
2020-08-11 14:23:21 -0700

    Created a componentWillUpdate lifecycle method that essentially checks for authentication in the dashboard via a HOC.

--- client/components/requireAuth.js
+++ client/components/requireAuth.js
@@ -3,12 +3,23 @@ import { graphql } from 'react-apollo';
 import currentUserQuery from '../queries/CurrentUser';
 import { hashHistory } from 'react-router';
 
-class RequireAuth extends Component {
-    componentDidMount() {
-        if (!this.props.data.loading && !this.props.data.user) {
-            hashHistory.push('/login');
+export default (WrappedComponent) => {
+    class RequireAuth extends Component {
+
+        componentWillUpdate(nextProps) {
+            if (!nextProps.data.loading && !nextProps.data.user) {
+                hashHistory.push('/login');
+            }
+        }
+        
+        render () {
+            return <WrappedComponent {...this.props} />;
         }
     }
-}
 
-graphql(currentUserQuery)(RequireAuth);
+
+
+
+return graphql(currentUserQuery)(RequireAuth);
+
+};
